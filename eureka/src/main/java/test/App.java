6c9a7c190cd94d52b962e1a2d6a8ee15f70334a5
郭强峰 <guoqiangfeng@ysstech.com>
2018-06-19 10:20:36 +0800

    提交注册中心

---
+++ eureka/src/main/java/test/App.java
@@ -0,0 +1,15 @@
+package test;
+
+import java.util.UUID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        java.util.UUID uuid = java.util.UUID.randomUUID();
+        System.out.println(uuid);
+        System.out.println( "Hello World!" );
+    }
+}
